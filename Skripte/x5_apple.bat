a4168babd59578116e465494ec4bf210edcc1d29
Djordje Radojkovic <Djordje.Radojkovic@protonmail.com>
2026-07-10 02:21:43 +0200

    Test skripte

---
+++ Skripte/x5_apple.bat
@@ -0,0 +1,7 @@
+@echo off
+:: This loop runs your command 5 times sequentially
+for /L %%i in (1, 1, 10) do (
+    echo Running iteration %%i
+    curl http://localhost:5050/search?keyword=apple
+)
+pause
